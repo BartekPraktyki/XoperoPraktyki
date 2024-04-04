12edf2168de86be057288c783397709113a99bb0
BartekPraktyki <164859023+BartekPraktyki@users.noreply.github.com>
2024-04-04 11:58:46 +0200

    Add files via upload

---
+++ talker/WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(100, 79);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Mów do mnie!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Powiedz to:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 34);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Liczba razy:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(86, 6);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(251, 23);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "Witaj!";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(86, 32);
+            numericUpDown1.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 4;
+            numericUpDown1.Value = new decimal(new int[] { 3, 0, 0, 0 });
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(349, 114);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private NumericUpDown numericUpDown1;
+    }
+}
---
+++ talker/WinFormsApp3/Talker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    internal class Talker
+    {
+        public static int BlahBlahBlah(string thingToSay, int numberOfTimes)
+        {
+            string FinalString = "";
+            for (int count = 1; count <= numberOfTimes; count++)
+            {
+                FinalString = FinalString + thingToSay + "\n";
+            }
+            MessageBox.Show(FinalString);
+            return FinalString.Length;
+        }
+    }
+}
