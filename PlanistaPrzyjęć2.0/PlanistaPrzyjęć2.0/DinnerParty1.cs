612817065ff6116f576a26a3cb5ee4780b842edb
BartekPraktyki <164859023+BartekPraktyki@users.noreply.github.com>
2024-04-15 09:52:43 +0200

    Add files via upload

---
+++ PlanistaPrzyjęć2.0/PlanistaPrzyjęć2.0/DinnerParty1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinnerParty1
+{
+    public class DinnerParty
+    {
+        public const int CostOfFoodPerPerson = 25;
+        public int NumberOfPeople { get; set; }
+        public bool FancyDecorations { get; set; }
+        public bool HealthyOption { get; set; }
+        public decimal CostOfBeveragesPerPerson;
+        public decimal CostOfDecorations = 0;
+
+        public DinnerParty(int numberOfPeople, bool fancyDecorations, bool healthyOption)
+        {
+            NumberOfPeople = numberOfPeople;
+            FancyDecorations = fancyDecorations;
+            HealthyOption = healthyOption;
+        }
+
+        public decimal CalculateCostOfDecorations()
+        {
+            decimal costOfDecarations;
+            if(FancyDecorations)
+            {
+                costOfDecarations = (NumberOfPeople * 15.00M) + 50M;
+            } else
+            {
+                costOfDecarations = (NumberOfPeople * 7.50M) + 30M;
+            }
+            return costOfDecarations;
+        }
+
+        private decimal CalculateCostOfBeveragesPerPerson()
+        {
+            decimal costOfBeveragesPerPerson;
+            if(HealthyOption)
+            {
+                costOfBeveragesPerPerson = 5.00M;
+            } else
+            {
+                costOfBeveragesPerPerson = 20.00M;
+            }
+            return costOfBeveragesPerPerson;
+
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                decimal totalCost = CalculateCostOfDecorations();
+                totalCost += ((CalculateCostOfBeveragesPerPerson() + CostOfFoodPerPerson) * NumberOfPeople);
+            if(HealthyOption)
+                {
+                    totalCost *= .95M;
+                }
+                return totalCost;
+            }
+
+        }
+
+        public decimal CalculateCost(bool healthyOption)
+        {
+            decimal totalCost = CostOfDecorations +
+                ((CostOfBeveragesPerPerson + CostOfFoodPerPerson) * NumberOfPeople);
+            if (healthyOption)
+            {
+                return totalCost * .95M;
+            }
+            else
+            {
+                return totalCost;
+            }
+        }
+    }
+}
---
+++ PlanistaPrzyjęć2.0/PlanistaPrzyjęć2.0/Form1.cs
@@ -0,0 +1,78 @@
+using DinnerParty1;
+
+namespace PlanistaPrzyjęć2._0
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerParty;
+        BirthdayParty birthdayParty;
+        public Form1()
+        {
+            InitializeComponent();
+            dinnerParty = new DinnerParty((int)numericUpDown1.Value,
+                healthyBox.Checked, fancyBox.Checked);
+
+            DisplayDinnerPartyCost();
+
+            birthdayParty = new BirthdayParty((int)numberBirthday.Value,
+                fancyBirthday.Checked, cakeWriting.Text);
+
+            DisplayBirthdayPartyCost();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.FancyDecorations = fancyBox.Checked;
+            DisplayDinnerPartyCost();
+        }
+
+        private void healthyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.HealthyOption = healthyBox.Checked;
+            DisplayDinnerPartyCost();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerParty.NumberOfPeople = (int)numericUpDown1.Value;
+            DisplayDinnerPartyCost();
+        }
+
+        private void DisplayDinnerPartyCost()
+        {
+            decimal Cost = dinnerParty.CalculateCost(healthyBox.Checked);
+            costLabel.Text = Cost.ToString("c");
+        }
+
+        private void fancyBirthday_CheckedChanged(object sender, EventArgs e)
+        {
+            birthdayParty.FancyDecorations = fancyBirthday.Checked;
+            DisplayBirthdayPartyCost();
+        }
+
+        private void numberBirthday_ValueChanged(object sender, EventArgs e)
+        {
+            birthdayParty.NumberOfPeople = (int)numberBirthday.Value;
+            DisplayBirthdayPartyCost();
+        }
+
+        private void DisplayBirthdayPartyCost()
+        {
+            TooLongLabel.Visible = birthdayParty.CakeWritingTooLong;
+            decimal Cost = birthdayParty.Cost;
+            birthdayCost.Text = Cost.ToString("c");
+        }
+
+        private void cakeWriting_TextChanged(object sender, EventArgs e)
+        {
+            birthdayParty.CakeWriting = cakeWriting.Text;
+            DisplayBirthdayPartyCost();
+        }
+
+    }
+}
