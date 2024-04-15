47da71053c4e4b0dff3f6761212cd6c8ad2d50a8
BartekPraktyki <164859023+BartekPraktyki@users.noreply.github.com>
2024-04-15 09:54:49 +0200

    Add files via upload

---
+++ AplikacjaKonsolowaJewels/AplikacjaKonsolowaJewels/JewelThief.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikacjaKonsolowaJewels
+{
+    internal class JewelThief : Locksmith
+    {
+        private Jewels stolenJewels = null;
+        public void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine("Kradnę zawartość sejfu! " + stolenJewels.Sparkle());
+        }
+    }
+}
