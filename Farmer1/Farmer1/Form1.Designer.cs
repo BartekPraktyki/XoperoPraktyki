469fc8c7dd331298f1eaba2fc667e7bd99a2ea16
BartekPraktyki <164859023+BartekPraktyki@users.noreply.github.com>
2024-04-15 09:51:54 +0200

    Add files via upload

---
+++ Farmer1/Farmer1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Farmer1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            calculate = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10F);
+            label1.Location = new Point(12, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Ilość krów";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(88, 7);
+            numericUpDown1.Maximum = new decimal(new int[] { 300, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 5, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(94, 23);
+            numericUpDown1.TabIndex = 1;
+            numericUpDown1.Value = new decimal(new int[] { 15, 0, 0, 0 });
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // calculate
+            // 
+            calculate.Location = new Point(56, 36);
+            calculate.Name = "calculate";
+            calculate.Size = new Size(75, 23);
+            calculate.TabIndex = 2;
+            calculate.Text = "Oblicz";
+            calculate.UseVisualStyleBackColor = true;
+            calculate.Click += calculate_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(204, 69);
+            Controls.Add(calculate);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private NumericUpDown numericUpDown1;
+        private Button calculate;
+    }
+}
---
+++ Farmer1/Farmer1/Form1.cs
@@ -0,0 +1,24 @@
+namespace Farmer1
+{
+    public partial class Form1 : Form
+    {
+        Farmer farmer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            farmer = new Farmer() { NumberOfCows = 15 };
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            farmer.NumberOfCows = (int)numericUpDown1.Value;
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Potrzebujê {0} worków paszy do wykarmienia {1} krów",
+                farmer.BagsOfFeed, farmer.NumberOfCows);
+        }
+    }
+}
