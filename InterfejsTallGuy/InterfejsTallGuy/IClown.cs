27d087b230427ee186de4c3ef906767aaa15f674
BartekPraktyki <164859023+BartekPraktyki@users.noreply.github.com>
2024-04-18 12:07:43 +0200

    Add files via upload

---
+++ InterfejsTallGuy/InterfejsTallGuy/IClown.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfejsTallGuy
+{
+    public interface IClown
+    {
+        string FunnyThingIHave
+        {
+            get { return "duże buty";  }
+        }
+        public void Honk() 
+        {
+            Console.WriteLine("Tut tuut!");
+        } 
+    }
+}
---
+++ InterfejsTallGuy/InterfejsTallGuy/Program.cs
@@ -0,0 +1,20 @@
+﻿using InterfejsTallGuy;
+
+public class TallGuy : IClown
+{
+    public string Name;
+    public int Height;
+
+    public void TalkAboutYourself()
+    {
+        Console.WriteLine("Nazywam się " + Name + " i mam " + Height + " centymetrów wzrostu.");
+    }
+    static void Main(string[] args)
+    {
+        TallGuy tallguy = new TallGuy() { Height = 74, Name = "Adam"};
+        tallguy.TalkAboutYourself();
+
+        
+        tallguy.Honk();
+    }
+}
