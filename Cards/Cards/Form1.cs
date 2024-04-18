0084450995f9a818b0a6a4e49bc1483e9e91f4d9
BartekPraktyki <164859023+BartekPraktyki@users.noreply.github.com>
2024-04-18 12:12:37 +0200

    Add files via upload

---
+++ Cards/Cards/Form1.cs
@@ -0,0 +1,17 @@
+namespace Cards
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Card card = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            MessageBox.Show(card.Name);
+        }
+    }
+}
