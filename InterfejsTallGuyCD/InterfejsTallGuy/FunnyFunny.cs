fae5a08d47cb7ecd8e96d900cdb5b29bbc4f87bc
BartekPraktyki <164859023+BartekPraktyki@users.noreply.github.com>
2024-04-18 12:08:39 +0200

    Add files via upload

---
+++ InterfejsTallGuyCD/InterfejsTallGuy/FunnyFunny.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfejsTallGuy
+{
+    class FunnyFunny : IClown
+    {
+        public FunnyFunny(string funnyThingIHave) {
+            this.funnyThingIHave = funnyThingIHave;
+        }
+        private string funnyThingIHave;
+        public string FunnyThingIHave
+        {
+            get { return "Cześć dzieciaki! Mam " + funnyThingIHave; }
+        }
+        public void Honk()
+        {
+            Console.WriteLine(this.FunnyThingIHave);
+        }
+    }
+}
---
+++ InterfejsTallGuyCD/InterfejsTallGuy/Program.cs
@@ -0,0 +1,13 @@
+﻿using InterfejsTallGuy;
+
+public class TallGuy
+{
+    static void Main(string[] args)
+    {
+        ScaryScary fingersTheClown = new ScaryScary("duże buty", 35);
+        FunnyFunny someFunnyClown = fingersTheClown;
+        IScaryClown someOtherScaryClown = someFunnyClown as ScaryScary;
+        someOtherScaryClown.Honk();
+        Console.ReadKey();
+    }
+}
