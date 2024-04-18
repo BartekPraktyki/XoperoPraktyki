df99384b40cfa6f5f7676a929822163f5a3fea4d
BartekPraktyki <164859023+BartekPraktyki@users.noreply.github.com>
2024-04-18 12:15:08 +0200

    Add files via upload

---
+++ IdzNaRyby/IdzNaRyby/Card.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdzNaRyby
+{
+    enum Suits
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts
+    }
+
+    enum Values
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    }
+    class Card
+    {
+        private Suits Suit { get; set; }
+        private Values Value { get; set; }
+
+        private Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+
+        private static string[] suits = new string[] { "pik", "trefl", "karo", "kier" };
+        private static string[] names = new string[]
+        {
+                "", "As", "Dwójka", "Trójka", "Czwórka", "Piątka", "Szóstka", "Siódemka", "Ósemka", "Dziewiątka", "Dziesiątka", "Walet", "Dama", "Król"
+        };
+
+        public string Name
+        {
+        get { return names[(int)Value] + " " + suits[(int)Suit]; }
+        }
+
+    }
+
+    public partial class Card
+    {
+        private static string[] names0 = new string[]
+        {
+            "", "asów", "dwójek", "trójek", "czwórek", "piątek", "szóstek", "siódemek", "ósemek", "dziewiątek", "dziesiątek", "waletów", "dam", "króli" 
+        };
+        private static string[] names1 = new string[]
+        {
+            "", "asa", "dwójkę", "trójkę", "czwórke", "piątke", "szóstke", "siódemke", "ósemke", "dziewiątke", "dziesiątke", "waleta", "damę", "króla"
+        };
+        private static string[] names2AndMore = new string[]
+        {
+            "", "asy", "dwójki", "trójki", "czwórki", "piątki", "szóstki", "siódemki", "ósemki", "dziewiątki", "dziesiątki", "walety", "damy", "króle"
+        };
+
+        public static string Plural(Card.Values value, int count)
+        {
+            if (count == 0)
+                return names0[(int)value];
+            if (count == 1)
+                return names1[(int)value];
+            return names2AndMore[(int)value];
+        }
+    }
+}
---
+++ IdzNaRyby/IdzNaRyby/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace IdzNaRyby
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textName = new TextBox();
+            buttonStart = new Button();
+            listHand = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            textProgress = new TextBox();
+            textBooks = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            buttonAsk = new Button();
+            SuspendLayout();
+            // 
+            // textName
+            // 
+            textName.Location = new Point(12, 32);
+            textName.Name = "textName";
+            textName.PlaceholderText = "Edek";
+            textName.Size = new Size(155, 23);
+            textName.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(173, 32);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(154, 23);
+            buttonStart.TabIndex = 1;
+            buttonStart.Text = "Rozpocznij grę!";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // listHand
+            // 
+            listHand.FormattingEnabled = true;
+            listHand.ItemHeight = 15;
+            listHand.Location = new Point(333, 31);
+            listHand.Name = "listHand";
+            listHand.Size = new Size(156, 379);
+            listHand.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Imię";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(333, 13);
+            label2.Name = "label2";
+            label2.Size = new Size(32, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Ręka";
+            // 
+            // textProgress
+            // 
+            textProgress.Location = new Point(12, 90);
+            textProgress.Multiline = true;
+            textProgress.Name = "textProgress";
+            textProgress.ReadOnly = true;
+            textProgress.Size = new Size(315, 253);
+            textProgress.TabIndex = 5;
+            // 
+            // textBooks
+            // 
+            textBooks.Location = new Point(12, 364);
+            textBooks.Multiline = true;
+            textBooks.Name = "textBooks";
+            textBooks.ReadOnly = true;
+            textBooks.Size = new Size(315, 74);
+            textBooks.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 72);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Postępy gry";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 346);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Grupy";
+            // 
+            // buttonAsk
+            // 
+            buttonAsk.Enabled = false;
+            buttonAsk.Location = new Point(333, 416);
+            buttonAsk.Name = "buttonAsk";
+            buttonAsk.Size = new Size(156, 23);
+            buttonAsk.TabIndex = 9;
+            buttonAsk.Text = "Zażądaj karty";
+            buttonAsk.UseVisualStyleBackColor = true;
+            buttonAsk.Click += buttonAsk_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(501, 450);
+            Controls.Add(buttonAsk);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBooks);
+            Controls.Add(textProgress);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listHand);
+            Controls.Add(buttonStart);
+            Controls.Add(textName);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textName;
+        private Button buttonStart;
+        private ListBox listHand;
+        private Label label1;
+        private Label label2;
+        private TextBox textProgress;
+        private TextBox textBooks;
+        private Label label3;
+        private Label label4;
+        private Button buttonAsk;
+    }
+}
