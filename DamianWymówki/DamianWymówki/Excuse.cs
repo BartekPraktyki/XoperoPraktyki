f4001e67235255f1044c8833cb4899515f8dbb46
BartekPraktyki <164859023+BartekPraktyki@users.noreply.github.com>
2024-04-19 09:43:43 +0200

    Add files via upload

---
+++ DamianWymówki/DamianWymówki/Excuse.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamianWymówki
+{
+    class Excuse
+    {
+        public string Description { get; set; }
+        public string Results { get; set; }
+        public DateTime LastUsed { get; set; }
+        public string ExcusePath { get; set; }
+        public Excuse()
+        {
+            ExcusePath = "";
+        }
+        public Excuse(string excusePath)
+        {
+            OpenFile(excusePath);
+        }
+        public Excuse(Random random, string folder)
+        {
+            string[] fileNames = Directory.GetFiles(folder, "*.txt");
+            OpenFile(fileNames[random.Next(fileNames.Length)]);
+        }
+        private void UpdateForm(bool changed)
+        {
+            if (!changed)
+            {
+                this.description.Text = currentExcuse.Description;
+                this.results.Text = currentExcuse.Results;
+                this.lastUsed.Text = currentExcuse.LastUsed;
+                if (!String.IsNullOrEmpty(currentExcuse.ExcusePath))
+                    fileDate.Text = File.GetLastWriteTime(currentExcuse.ExcusePath).ToString();
+                this.Text = "Program do zarządzania wymówkami";
+            }
+            else
+                this.Text = "Program do zarządzania wymówkami";
+            this.formChanged = changed;
+        }
+        private void OpenFile(string excusePath)
+        {
+            this.ExcusePath = excusePath;
+            using (StreamReader reader = new StreamReader(excusePath))
+            {
+                Description = reader.ReadLine();
+                Results = reader.ReadLine();
+                LastUsed = Convert.ToDateTime(reader.ReadLine());
+            }
+        }
+
+        private void Save(string fileName)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine(Description);
+                writer.WriteLine(Results);
+                writer.WriteLine(LastUsed);
+            }
+        }
+    }
+}
